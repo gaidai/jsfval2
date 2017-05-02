ff13a01b7bac8311a565e3b82bfe0a6cd72ff02f
gaidai <q-daddy@mail.ru>
2017-05-02 23:29:28 +0300

    First JSF with  f:validateLongRange

---
+++ src/main/java/com/sgaidai/jsfval/ValMas.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sgaidai.jsfval;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+
+@ManagedBean(name="mas")
+@SessionScoped
+public class ValMas implements Serializable{
+    
+    String uiinput = " Value is FALSE.";
+
+    public String getUiinput() {
+        return uiinput;
+    }
+
+    public void setUiinput(String uiinput) {
+        this.uiinput = uiinput;
+    }
+}
